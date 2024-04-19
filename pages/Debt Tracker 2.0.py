4a5685832ec99fad8f210ce0ba185edfeaa4903a
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-04-19 12:37:08 -0400

    html to pdf?

--- pages/Debt Tracker 2.0.py
+++ pages/Debt Tracker 2.0.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import os
-#import sys
-#sys.path.insert(0, r"C:\Users\DSikkink\OneDrive - US House of Representatives\Python\Essential Numbers\workhorses")
+import pdfkit
 from workhorses import cool_debt_metrics as cdm
 
+
+# Add a button to download the HTML as a PDF
+pdf = pdfkit.from_string(cdm.html, False)
+st.download_button(
+        "⬇️ Download PDF",
+        data=pdf,
+        file_name=f"Debt Tracking Report {cdm.today}.pdf",
+        mime="application/octet-stream"
+    )
+
 components.html(cdm.html, scrolling=False, height=4500, width=1000)
+
