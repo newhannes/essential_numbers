70eda937d8f3411b8d6f32d07368eae3b22b61ad
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-04-30 16:01:47 -0400

    .

--- pages/Static_Debt_Tracker.py
+++ pages/Static_Debt_Tracker.py
@@ -128,7 +128,7 @@ html = f"""
 """
 
 # Convert the HTML file to a PDF
-pdf = pdfkit.from_string(html, False)
+pdf = pdfkit.from_string(html, False, options={"enable-local-file-access": ""})
 
 st.title("Static Debt Tracker")
 
