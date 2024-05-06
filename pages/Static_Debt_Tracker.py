e03f21a47a73b2e625697fd4f03024f01892a856
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-05-06 12:34:25 -0400

    making it liveupdate

--- pages/Static_Debt_Tracker.py
+++ pages/Static_Debt_Tracker.py
@@ -1,11 +1,12 @@
 ###### ------------------ STATIC MAIN FILE, PRODUCES HTML AND PDFS ------------------ ######
 import streamlit as st
 import static_debt_tracker.static_cool_debt_metrics as cdm
-from workhorses import debt_tracker as dt
+from workhorses.debt_tracker import debt_tracker_main
 import pdfkit
 import streamlit.components.v1 as components
 import shutil
 
+dt = debt_tracker_main()
 
 #write the house budget header from inputs/HBR_Logo_Primary.png to the temp directory
 # Define the source and destination paths
@@ -19,35 +20,35 @@ basic_debt_html = f"""
 <div class="container">
 <div class="content">
     <h2>Current Debt</h2>
-    <p>The gross national debt is currently <strong>${dt.current_debt_rounded:,} trillion</strong>. This equates to:</p>
+    <p>The gross national debt is currently <strong>${dt['current_debt_rounded']:,} trillion</strong>. This equates to:</p>
     <ul>
-        <li><strong>${dt.debt_per_person:,}</strong> per person</li>
-        <li><strong>${dt.debt_per_household:,}</strong> per household</li>
-        <li><strong>${dt.debt_per_child:,}</strong> per child</li>
+        <li><strong>${dt['debt_per_person']:,}</strong> per person</li>
+        <li><strong>${dt['debt_per_household']:,}</strong> per household</li>
+        <li><strong>${dt['debt_per_child']:,}</strong> per child</li>
     </ul>
 
     <h2>Debt Accumulation under President Biden</h2>
-    <p>When President Biden took office total gross debt was <strong>${dt.biden_start_debt_rounded:,} trillion</strong>, meaning he has increased the national debt by <strong>${dt.biden_debt_rounded:,} trillion</strong>. This equates to:</p>
+    <p>When President Biden took office total gross debt was <strong>${dt['biden_start_debt_rounded']:,} trillion</strong>, meaning he has increased the national debt by <strong>${dt['biden_debt_rounded']:,} trillion</strong>. This equates to:</p>
     <ul>
-        <li><strong>${dt.biden_debt_per_person:,}</strong> more debt per person</li>
-        <li><strong>${dt.biden_debt_per_household:,}</strong> more debt per household</li>
-        <li><strong>${dt.biden_debt_per_child:,}</strong> more debt per child</li>
+        <li><strong>${dt['biden_debt_per_person']:,}</strong> more debt per person</li>
+        <li><strong>${dt['biden_debt_per_household']:,}</strong> more debt per household</li>
+        <li><strong>${dt['biden_debt_per_child']:,}</strong> more debt per child</li>
     </ul>
     <p>The rate of debt accumulation during the Biden Administration has equaled:</p>
     <ul>
-        <li><strong>${dt.biden_debt_per_day_rounded:,} billion</strong> in new debt per day</li>
-        <li><strong>${dt.biden_debt_per_hour:,} million</strong> in new debt per hour</li>
-        <li><strong>${dt.biden_debt_per_min:,} million</strong> in new debt per minute</li>
-        <li><strong>${dt.biden_debt_per_sec:,}</strong> in new debt per second</li>
+        <li><strong>${dt['biden_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
+        <li><strong>${dt['biden_debt_per_hour']:,} million</strong> in new debt per hour</li>
+        <li><strong>${dt['biden_debt_per_min']:,} million</strong> in new debt per minute</li>
+        <li><strong>${dt['biden_debt_per_sec']:,}</strong> in new debt per second</li>
     </ul>
 
     <h2>Debt Accumulation in Past Year</h2>
-    <p>The debt one year ago was <strong>${dt.debt_year_ago_rounded:,} trillion</strong>, meaning that the debt has increased by <strong>${dt.debt_increase_from_year_ago_rounded:,} trillion</strong> over the past 12 months. This rate of increase equates to:</p>
+    <p>The debt one year ago was <strong>${dt['debt_year_ago_rounded']:,} trillion</strong>, meaning that the debt has increased by <strong>${dt['debt_increase_from_year_ago_rounded']:,} trillion</strong> over the past 12 months. This rate of increase equates to:</p>
     <ul>
-        <li><strong>${dt.last_year_debt_per_day_rounded:,} billion</strong> in new debt per day</li>
-        <li><strong>${dt.last_year_debt_per_hour:,} million</strong> in new debt per hour</li>
-        <li><strong>${dt.last_year_debt_per_min:,} million</strong> in new debt per minute</li>
-        <li><strong>${dt.last_year_debt_per_sec:,}</strong> in new debt per second</li>
+        <li><strong>${dt['last_year_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
+        <li><strong>${dt['last_year_debt_per_hour']:,} million</strong> in new debt per hour</li>
+        <li><strong>${dt['last_year_debt_per_min']:,} million</strong> in new debt per minute</li>
+        <li><strong>${dt['last_year_debt_per_sec']:,}</strong> in new debt per second</li>
     </ul>
 </div>
 </div>
