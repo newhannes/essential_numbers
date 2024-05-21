5848fafb736865aff23c934f956ff2d8843d120b
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-05-21 11:30:17 -0400

    ,

--- pages/Debt_Tracker.py
+++ pages/Debt_Tracker.py
@@ -30,6 +30,47 @@ today_string = day_string_formatter(dt['today'])
 
 
 basic_debt_html = f"""
+<h1 style='text-align: center;'>Debt Tracker</h1>
+<h3 style='text-align: center;'> As of {today_string}</h3>
+<div class="container">
+    <div class="content">
+        <h2>Current Debt</h2>
+        <p>The gross national debt is currently <strong>${dt['current_debt_rounded']:,} trillion</strong>. This equates to:</p>
+        <ul>
+            <li><strong>${dt['debt_per_person']:,}</strong> per person</li>
+            <li><strong>${dt['debt_per_household']:,}</strong> per household</li>
+            <li><strong>${dt['debt_per_child']:,}</strong> per child</li>
+        </ul>
+        <h2>Debt Accumulation under President Biden</h2>
+        <p>When President Biden took office total gross debt was <strong>${dt['biden_start_debt_rounded']:,} trillion</strong>, meaning he has increased the national debt by <strong>${dt['biden_debt_rounded']:,} trillion</strong>. This equates to:</p>
+        <ul>
+            <li><strong>${dt['biden_debt_per_person']:,}</strong> more debt per person</li>
+            <li><strong>${dt['biden_debt_per_household']:,}</strong> more debt per household</li>
+            <li><strong>${dt['biden_debt_per_child']:,}</strong> more debt per child</li>
+        </ul>
+        <p>The rate of debt accumulation during the Biden Administration has equaled:</p>
+        <ul>
+            <li><strong>${dt['biden_debt_per_month']:,} billion</strong> in new debt per month</li>
+            <li><strong>${dt['biden_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
+            <li><strong>${dt['biden_debt_per_hour']:,} million</strong> in new debt per hour</li>
+            <li><strong>${dt['biden_debt_per_min']:,} million</strong> in new debt per minute</li>
+            <li><strong>${dt['biden_debt_per_sec']:,}</strong> in new debt per second</li>
+        </ul>
+        <h2>Debt Accumulation in Past Year</h2>
+        <p>The debt one year ago was <strong>${dt['debt_year_ago_rounded']:,} trillion</strong>, meaning that the debt has increased by <strong>${dt['debt_increase_from_year_ago_rounded']:,} trillion</strong> over the past 12 months. This rate of increase equates to:</p>
+        <ul>
+            <li><strong>${dt['last_year_debt_per_month']:,} billion</strong> in new debt per month</li>
+            <li><strong>${dt['last_year_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
+            <li><strong>${dt['last_year_debt_per_hour']:,} million</strong> in new debt per hour</li>
+            <li><strong>${dt['last_year_debt_per_min']:,} million</strong> in new debt per minute</li>
+            <li><strong>${dt['last_year_debt_per_sec']:,}</strong> in new debt per second</li>
+        </ul>
+    </div>
+</div>
+"""
+st.markdown(basic_debt_html, unsafe_allow_html=True)
+
+html_for_pdf = f"""
 <!DOCTYPE html>
 <html>
     <style>
@@ -66,49 +107,13 @@ basic_debt_html = f"""
             margin-top: 20px;
         }}
     </style>
-<h1 style='text-align: center; color: black;'>Debt Tracker</h1>
-<h3 style='text-align: center; color: black;'> As of {today_string}</h3>
-<div class="container">
-    <div class="content">
-        <h2>Current Debt</h2>
-        <p>The gross national debt is currently <strong>${dt['current_debt_rounded']:,} trillion</strong>. This equates to:</p>
-        <ul>
-            <li><strong>${dt['debt_per_person']:,}</strong> per person</li>
-            <li><strong>${dt['debt_per_household']:,}</strong> per household</li>
-            <li><strong>${dt['debt_per_child']:,}</strong> per child</li>
-        </ul>
-        <h2>Debt Accumulation under President Biden</h2>
-        <p>When President Biden took office total gross debt was <strong>${dt['biden_start_debt_rounded']:,} trillion</strong>, meaning he has increased the national debt by <strong>${dt['biden_debt_rounded']:,} trillion</strong>. This equates to:</p>
-        <ul>
-            <li><strong>${dt['biden_debt_per_person']:,}</strong> more debt per person</li>
-            <li><strong>${dt['biden_debt_per_household']:,}</strong> more debt per household</li>
-            <li><strong>${dt['biden_debt_per_child']:,}</strong> more debt per child</li>
-        </ul>
-        <p>The rate of debt accumulation during the Biden Administration has equaled:</p>
-        <ul>
-            <li><strong>${dt['biden_debt_per_month']:,} billion</strong> in new debt per month</li>
-            <li><strong>${dt['biden_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
-            <li><strong>${dt['biden_debt_per_hour']:,} million</strong> in new debt per hour</li>
-            <li><strong>${dt['biden_debt_per_min']:,} million</strong> in new debt per minute</li>
-            <li><strong>${dt['biden_debt_per_sec']:,}</strong> in new debt per second</li>
-        </ul>
-        <h2>Debt Accumulation in Past Year</h2>
-        <p>The debt one year ago was <strong>${dt['debt_year_ago_rounded']:,} trillion</strong>, meaning that the debt has increased by <strong>${dt['debt_increase_from_year_ago_rounded']:,} trillion</strong> over the past 12 months. This rate of increase equates to:</p>
-        <ul>
-            <li><strong>${dt['last_year_debt_per_month']:,} billion</strong> in new debt per month</li>
-            <li><strong>${dt['last_year_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
-            <li><strong>${dt['last_year_debt_per_hour']:,} million</strong> in new debt per hour</li>
-            <li><strong>${dt['last_year_debt_per_min']:,} million</strong> in new debt per minute</li>
-            <li><strong>${dt['last_year_debt_per_sec']:,}</strong> in new debt per second</li>
-        </ul>
-    </div>
-</div>
+    <h1 style='text-align: center; color: black;'>Debt Tracker</h1>
+        <h3 style='text-align: center; color: black;'> As of {today_string}</h3>
+        {basic_debt_html}
 </html>
 """
-st.markdown(basic_debt_html, unsafe_allow_html=True)
-
 # Convert the HTML file to a PDF
-pdf = pdfkit.from_string(basic_debt_html, False, options={"enable-local-file-access": ""})
+pdf = pdfkit.from_string(html_for_pdf, False, options={"enable-local-file-access": ""})
 
 # Add a button to download the PDF
 st.download_button(
