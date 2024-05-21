de03ca298b71b5a466fb70b70a9c9f6c49688746
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-05-21 11:05:19 -0400

    reform to html

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This project has two purposes. The primary purpose is to provide the rest of HBC 24/7 access to live-updating stats. The secondary purpose is to develop products to enhance the various stats we track. These products have two requirements: visually pleasing and impactful. 
 
 ## Current TODO
-- DEBT TRACKER: add monthly numbers, more rounding, create PDF downloadd
+- DEBT TRACKER: add HBC banner, create PDF download
 - Add different fonts (Garet, Playfair Display)
 - Create "Bidenomics" Stats page
 - Create Econ Summary Stats page
--- pages/Debt_Tracker.py
+++ pages/Debt_Tracker.py
@@ -19,37 +19,77 @@ if dt['today'] != datetime.today():
                 dt = debt_tracker_main()
 
 today_string = day_string_formatter(dt['today'])
-st.markdown("<h1 style='text-align: center; color: black;'>Debt Tracker</h1>", unsafe_allow_html=True)
-st.markdown(f"<h3 style='text-align: center; color: black;'> As of {today_string}</h3>", unsafe_allow_html=True)
+# st.markdown("<h1 style='text-align: center; color: black;'>Debt Tracker</h1>", unsafe_allow_html=True)
+# st.markdown(f"<h3 style='text-align: center; color: black;'> As of {today_string}</h3>", unsafe_allow_html=True)
 
-# Current Debt
-st.markdown("<h2>Current Debt</h2>", unsafe_allow_html=True)
-st.write(f"The gross national debt is currently **${dt['current_debt_rounded']:,} trillion**. This equates to:")
-st.markdown(f""" - **\${dt['debt_per_person']:,}** per person""") 
-st.markdown(f"- **\${dt['debt_per_household']:,}** per household")
-st.markdown(f"- **\${dt['debt_per_child']:,}** per child")
-# Biden Debt
-st.markdown("<h2>Biden Debt</h2>", unsafe_allow_html=True)
-st.write(f"When President Biden took office total gross debt was **\${dt['biden_start_debt_rounded']:,} trillion**, meaning he has increased the national debt by **${dt['biden_debt_rounded']:,} trillion**. This equates to:")
-st.markdown(f"- **${dt['biden_debt_per_person']:,}** more debt per person")
-st.markdown(f"- **${dt['biden_debt_per_household']:,}** more debt per household")
-st.markdown(f"- **${dt['biden_debt_per_child']:,}** more debt per child")
-# Biden Debt Accumulation
-st.write("The rate of debt accumulation during the Biden Administration has equaled:")
-st.markdown(f"- **${dt['biden_debt_per_month']:,} billion** in new debt per month")
-st.markdown(f"- **${dt['biden_debt_per_day_rounded']:,} billion** in new debt per day")
-st.markdown(f"- **${dt['biden_debt_per_hour']:,} million** in new debt per hour")
-st.markdown(f"- **${dt['biden_debt_per_min']:,} million** in new debt per minute")
-st.markdown(f"- **${dt['biden_debt_per_sec']:,}** in new debt per second")
-# Debt Accumulation in Past Year
-st.markdown("<h2>Debt Accumulation in Past Year</h2>", unsafe_allow_html=True)
-st.write(f"""The debt one year ago was **\${dt['debt_year_ago_rounded']:,} trillion**, meaning that the debt has increased by **${dt['debt_increase_from_year_ago_rounded']:,} trillion**
-        over the past 12 months. This rate of increase equates to:""")
-st.markdown(f"- **${dt['last_year_debt_per_month']:,} billion** in new debt per month")
-st.markdown(f"- **${dt['last_year_debt_per_day_rounded']:,} billion** in new debt per day")
-st.markdown(f"- **${dt['last_year_debt_per_hour']:,} million** in new debt per hour")
-st.markdown(f"- **${dt['last_year_debt_per_min']:,} million** in new debt per minute")
-st.markdown(f"- **${dt['last_year_debt_per_sec']:,}** in new debt per second")
+# # Current Debt
+# st.markdown("<h2>Current Debt</h2>", unsafe_allow_html=True)
+# st.write(f"The gross national debt is currently **${dt['current_debt_rounded']:,} trillion**. This equates to:")
+# st.markdown(f""" - **\${dt['debt_per_person']:,}** per person""") 
+# st.markdown(f"- **\${dt['debt_per_household']:,}** per household")
+# st.markdown(f"- **\${dt['debt_per_child']:,}** per child")
+# # Biden Debt
+# st.markdown("<h2>Biden Debt</h2>", unsafe_allow_html=True)
+# st.write(f"When President Biden took office total gross debt was **\${dt['biden_start_debt_rounded']:,} trillion**, meaning he has increased the national debt by **${dt['biden_debt_rounded']:,} trillion**. This equates to:")
+# st.markdown(f"- **${dt['biden_debt_per_person']:,}** more debt per person")
+# st.markdown(f"- **${dt['biden_debt_per_household']:,}** more debt per household")
+# st.markdown(f"- **${dt['biden_debt_per_child']:,}** more debt per child")
+# # Biden Debt Accumulation
+# st.write("The rate of debt accumulation during the Biden Administration has equaled:")
+# st.markdown(f"- **${dt['biden_debt_per_month']:,} billion** in new debt per month")
+# st.markdown(f"- **${dt['biden_debt_per_day_rounded']:,} billion** in new debt per day")
+# st.markdown(f"- **${dt['biden_debt_per_hour']:,} million** in new debt per hour")
+# st.markdown(f"- **${dt['biden_debt_per_min']:,} million** in new debt per minute")
+# st.markdown(f"- **${dt['biden_debt_per_sec']:,}** in new debt per second")
+# # Debt Accumulation in Past Year
+# st.markdown("<h2>Debt Accumulation in Past Year</h2>", unsafe_allow_html=True)
+# st.write(f"""The debt one year ago was **\${dt['debt_year_ago_rounded']:,} trillion**, meaning that the debt has increased by **${dt['debt_increase_from_year_ago_rounded']:,} trillion**
+#         over the past 12 months. This rate of increase equates to:""")
+# st.markdown(f"- **${dt['last_year_debt_per_month']:,} billion** in new debt per month")
+# st.markdown(f"- **${dt['last_year_debt_per_day_rounded']:,} billion** in new debt per day")
+# st.markdown(f"- **${dt['last_year_debt_per_hour']:,} million** in new debt per hour")
+# st.markdown(f"- **${dt['last_year_debt_per_min']:,} million** in new debt per minute")
+# st.markdown(f"- **${dt['last_year_debt_per_sec']:,}** in new debt per second")
 
         
-        
+basic_debt_html = f"""
+<img src={'inputs/HBR_Logo_Primary.png'} width="500" align = "middle">
+<h1 style='text-align: center; color: black;'>Debt Tracker</h1>
+<h3 style='text-align: center; color: black;'> As of {today_string}</h3>
+<div class="container">
+    <div class="content">
+        <h2>Current Debt</h2>
+        <p>The gross national debt is currently <strong>${dt['current_debt_rounded']:,} trillion</strong>. This equates to:</p>
+        <ul>
+            <li><strong>${dt['debt_per_person']:,}</strong> per person</li>
+            <li><strong>${dt['debt_per_household']:,}</strong> per household</li>
+            <li><strong>${dt['debt_per_child']:,}</strong> per child</li>
+        </ul>
+        <h2>Debt Accumulation under President Biden</h2>
+        <p>When President Biden took office total gross debt was <strong>${dt['biden_start_debt_rounded']:,} trillion</strong>, meaning he has increased the national debt by <strong>${dt['biden_debt_rounded']:,} trillion</strong>. This equates to:</p>
+        <ul>
+            <li><strong>${dt['biden_debt_per_person']:,}</strong> more debt per person</li>
+            <li><strong>${dt['biden_debt_per_household']:,}</strong> more debt per household</li>
+            <li><strong>${dt['biden_debt_per_child']:,}</strong> more debt per child</li>
+        </ul>
+        <p>The rate of debt accumulation during the Biden Administration has equaled:</p>
+        <ul>
+            <li><strong>${dt['biden_debt_per_month']:,} billion</strong> in new debt per month</li>
+            <li><strong>${dt['biden_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
+            <li><strong>${dt['biden_debt_per_hour']:,} million</strong> in new debt per hour</li>
+            <li><strong>${dt['biden_debt_per_min']:,} million</strong> in new debt per minute</li>
+            <li><strong>${dt['biden_debt_per_sec']:,}</strong> in new debt per second</li>
+        </ul>
+        <h2>Debt Accumulation in Past Year</h2>
+        <p>The debt one year ago was <strong>${dt['debt_year_ago_rounded']:,} trillion</strong>, meaning that the debt has increased by <strong>${dt['debt_increase_from_year_ago_rounded']:,} trillion</strong> over the past 12 months. This rate of increase equates to:</p>
+        <ul>
+            <li><strong>${dt['last_year_debt_per_month']:,} billion</strong> in new debt per month</li>
+            <li><strong>${dt['last_year_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
+            <li><strong>${dt['last_year_debt_per_hour']:,} million</strong> in new debt per hour</li>
+            <li><strong>${dt['last_year_debt_per_min']:,} million</strong> in new debt per minute</li>
+            <li><strong>${dt['last_year_debt_per_sec']:,}</strong> in new debt per second</li>
+        </ul>
+    </div>
+</div>
+"""
+st.markdown(basic_debt_html, unsafe_allow_html=True)
