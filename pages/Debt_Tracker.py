da5d3dd1987b44cf8aef322f99975a7904b2f709
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-09-23 09:35:35 -0400

    switched to Biden-Harris

--- pages/Debt_Tracker.py
+++ pages/Debt_Tracker.py
@@ -41,14 +41,14 @@ basic_debt_html = f"""
             <li><strong>${dt['debt_per_household']:,}</strong> per household</li>
             <li><strong>${dt['debt_per_child']:,}</strong> per child</li>
         </ul>
-        <h2>Debt Accumulation under President Biden</h2>
-        <p>When President Biden took office total gross debt was <strong>${dt['biden_start_debt_rounded']:,} trillion</strong>, meaning he has increased the national debt by <strong>${dt['biden_debt_rounded']:,} trillion</strong>. This equates to:</p>
+        <h2>Debt Accumulation under President Biden and VP Harris</h2>
+        <p>When President Biden and VP Harris took office total gross debt was <strong>${dt['biden_start_debt_rounded']:,} trillion</strong>, meaning they have increased the national debt by <strong>${dt['biden_debt_rounded']:,} trillion</strong>. This equates to:</p>
         <ul>
             <li><strong>${dt['biden_debt_per_person']:,}</strong> more debt per person</li>
             <li><strong>${dt['biden_debt_per_household']:,}</strong> more debt per household</li>
             <li><strong>${dt['biden_debt_per_child']:,}</strong> more debt per child</li>
         </ul>
-        <p>The rate of debt accumulation during the Biden Administration has equaled:</p>
+        <p>The rate of debt accumulation during the Biden-Harris Administration has equaled:</p>
         <ul>
             <li><strong>${dt['biden_debt_per_month']:,} billion</strong> in new debt per month</li>
             <li><strong>${dt['biden_debt_per_day_rounded']:,} billion</strong> in new debt per day</li>
