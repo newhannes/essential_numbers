4ec3c8a219949285574dfdb8001538bd37dd15cb
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-09-23 10:13:51 -0400

    trying st.navigation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,18 @@
 import streamlit as st
-from st_pages import Page, add_page_title, show_pages
+# from st_pages import Page, add_page_title, show_pages
 
-show_pages(
-    [
-        Page("pages/Home.py", "Home"),
-        Page("pages/Debt_Tracker.py", "Debt Tracker"),
-        Page("pages/Static_Debt_Tracker.py", "Static Debt Tracker"),
-        Page("pages/DebtTrackerV3.py", "Debt Tracker V3"),
-        Page("pages/JCA_Quick_Stats.py", "JCA Favorites"),
-        Page("pages/Labor_Data.py", "Employment and Wages"),
-        Page("pages/Inflation.py", "Inflation"),
-        Page("pages/About.py", "About")
-    ]
-)
+# show_pages(
+#     [
+#         Page("pages/Home.py", "Home"),
+#         Page("pages/Debt_Tracker.py", "Debt Tracker"),
+#         Page("pages/Static_Debt_Tracker.py", "Static Debt Tracker"),
+#         Page("pages/DebtTrackerV3.py", "Debt Tracker V3"),
+#         Page("pages/JCA_Quick_Stats.py", "JCA Favorites"),
+#         Page("pages/Labor_Data.py", "Employment and Wages"),
+#         Page("pages/Inflation.py", "Inflation"),
+#         Page("pages/About.py", "About")
+#     ]
+# )
 
+page = st.navigation([st.page("pages/Home.py"), st.Page("pages/Debt_Tracker.py")])
+page.run()
