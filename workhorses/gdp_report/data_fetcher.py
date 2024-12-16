19986042850b2c474080ed9aa06b35d595671945
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-12-16 15:18:26 -0500

    replaced beaapi requirement with pure python API call

--- workhorses/gdp_report/data_fetcher.py
+++ workhorses/gdp_report/data_fetcher.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import requests
 import time
-import beaapi
 import os
 import streamlit as st
 
 BEA_API_KEY = st.secrets('BEA_API_KEY')
 
+
+def bea_api_fetch(api_key, dataset_name, table_name, frequency, year):
+    url = (f"https://apps.bea.gov/api/data/?&UserID={api_key}&method=GetData&DataSetName="
+	   f"{dataset_name}&TableName={table_name}&Frequency={frequency}&Year={year}&ResultFormat=json")
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    data_df = pd.DataFrame(data['BEAAPI']['Results']['Data'])
+    return data_df
+
 ## Data Helper Functions
 def get_data_with_retry(api_key, datasetname, TableName, Frequency, Year, retries=3, delay=5):
     for attempt in range(retries):
         try:
-            return beaapi.get_data(api_key, datasetname=datasetname, TableName=TableName, Frequency=Frequency, Year=Year)
+            return bea_api_fetch(api_key, datasetname, TableName, Frequency, Year)
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 503:
                 print(f"Service unavailable. Retrying in {delay} seconds...")
