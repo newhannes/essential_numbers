adb5d18374f2fef8f16364a172f3433a446c0732
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-12-16 14:51:49 -0500

    run page in streamlit_main

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ pg = st.navigation(
     st.Page("app_pages/Inflation.py"),
     st.Page("app_pages/Labor_Data.py"),
     st.Page("app_pages/JCA_Quick_Stats.py"),
-    st.Page("app_pages/Summary_Stats.py")
+    st.Page("app_pages/Summary_Stats.py"),
+    st.Page("app_pages/GDP_Report.py")
     ]
     )
 
