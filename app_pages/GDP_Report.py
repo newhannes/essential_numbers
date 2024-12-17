4c7e76ef39b35235f8b99f17eb0db66be1ce265e
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-12-17 16:04:26 -0500

    display pdf (cause the html isn't working in the display)

--- app_pages/GDP_Report.py
+++ app_pages/GDP_Report.py
@@ -2,6 +2,7 @@ import pdfkit
 import pandas as pd
 from workhorses.gdp_report.main import main as gdp_report_main
 import streamlit as st
+import base64
 
 with st.spinner("Generating GDP Report..."):
     html_string = gdp_report_main()
@@ -17,8 +18,10 @@ st.image(image_path)
 pdf = pdfkit.from_string(html_string, False, options={"enable-local-file-access": ""})
 
 # Show the pdf 
-st.html(f'<embed src="data:application/pdf;base64,{pdf}" width="100%" height="800"></embed>')
-
+with open(pdf, "rb") as f:
+    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+st.markdown(pdf_display, unsafe_allow_html=True)
 
 cols = st.columns([1, 1, 1])
 with cols[1]:
