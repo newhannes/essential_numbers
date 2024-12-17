f40aabe215d436e6405cf331c38dd78a84506639
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2024-12-17 16:59:44 -0500

    added eb garamond to packages via debian

--- workhorses/gdp_report/visualizations.py
+++ workhorses/gdp_report/visualizations.py
@@ -46,7 +46,7 @@ def overview_chart(pct_change_raw, current_q):
 
     ## Setup
     apply_style()
-    plt.rcParams['font.family'] = CUSTOM_FONT_NAME
+    plt.rcParams['font.family'] = "EB Garamond"
     fig, ax = plt.subplots(figsize=(8,3.5))
     fig.set_facecolor(chart_background)
     ax.set_facecolor(chart_background)
