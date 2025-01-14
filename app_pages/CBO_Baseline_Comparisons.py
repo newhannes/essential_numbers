91181c8db44c755072d0c5cf4931ff5630b99e6a
Drew-WorkDesktop <drew.sikkink@mail.house.gov>
2025-01-14 14:55:16 -0500

    test

--- app_pages/CBO_Baseline_Comparisons.py
+++ app_pages/CBO_Baseline_Comparisons.py
@@ -6,6 +6,9 @@
 import streamlit as st
 from workhorses.comparing_baselines import get_mega_baseline_dataframe, compare_current_baseline_to_previous, get_baseline_data
 
+# Preparation
+# -----------------
+
 # Load the data
 cbo_csv_url = "https://raw.githubusercontent.com/US-CBO/eval-projections/refs/heads/main/input_data/baselines.csv"
 baselines = get_mega_baseline_dataframe(cbo_csv_url)
@@ -21,11 +24,23 @@ categories = current_baseline.category.unique()
 subcategories = current_baseline.subcategory.unique()
 projected_years = current_baseline.projected_fiscal_year.unique()
 
+
+# Streamlit Page
+# -------------------
+st.title("CBO Baseline Comparisons")
+
 # User input for component, category, and subcategory
 component = st.selectbox("Select a component", components)
-category = st.selectbox("Select a category", categories)
-subcategory = st.selectbox("Select a subcategory", subcategories)
-projected_yr = st.selectbox("Select a year", projected_years)
+
+if component:
+    category = st.selectbox("Select a category", current_baseline.query(f"component == '{component}'").category.unique())
+if category:
+    subcategory = st.selectbox("Select a subcategory", current_baseline.query(f"component == '{component}' and category == '{category}'").subcategory.unique())
+if subcategory:
+    projected_yr = st.selectbox("Select a year", current_baseline.query(f"component == '{component}' and category == '{category}' and subcategory == '{subcategory}'").projected_fiscal_year.unique())
+
+# subcategory = st.selectbox("Select a subcategory", subcategories)
+# projected_yr = st.selectbox("Select a year", projected_years)
 ten_year_sum = st.checkbox("Sum over 10 years")
 
 # Get the comparison
@@ -34,8 +49,7 @@ current_val, prior_val = compare_current_baseline_to_previous(current_baseline, 
                                                               projected_yr, ten_year_sum=ten_year_sum) 
 
 # Display the comparison
-st.title("CBO Baseline Comparisons")
-st.write(f"Current Value: {current_val}")
-st.write(f"Prior Value: {prior_val}")
+st.write(f"Current Value: ${current_val:,.0f}")
+st.write(f"Prior Value: {prior_val:,.0f}")
 st.write(f"Difference: {current_val - prior_val:,.0f}")
 st.write(f"Percent Change: {((current_val - prior_val) / prior_val) * 100:.1f}%")
